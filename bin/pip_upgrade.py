0f5ce350c0c7cdcefa1edc7b61f8c60dfb8eccf0
Kushal Shrestha <81743741+k-stha@users.noreply.github.com>
2022-02-17 01:06:45 -0500

    Initial commit

---
+++ bin/pip_upgrade.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""Automatically upgrade all python packages."""
+
+import os
+from datetime import datetime
+from platform import system
+from subprocess import call, run
+
+from pkg_resources import working_set
+
+
+def create_dir(directory):
+    """Create specified directory if it does not exist."""
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+
+
+PIP="pip3"
+
+HOME = os.path.expanduser("~")
+
+if system() == "Windows":
+    LOGDIR = HOME + "\\AppData\\Local\\pip_upgrade\\"
+    create_dir(LOGDIR)
+else:
+    LOGDIR = HOME + "/.cache/pip_upgrade/"
+    create_dir(LOGDIR)
+
+LOGFILE = LOGDIR + "prev_pkg_versions.log"
+
+# pylint: disable-next=not-an-iterable
+packages = [package.project_name for package in working_set]
+
+pkglist = run([PIP, "freeze", "--all"], capture_output=True, check=True).stdout
+
+with open(LOGFILE, "a", encoding="utf-8") as log:
+    log.write("-" * 26 + "\n" + str(datetime.now()) + "\n" + "-" * 26 + "\n")
+    log.write(pkglist.decode("utf-8"))
+log.close()
+
+call(PIP + "install --upgrade " + " ".join(packages), shell=True)
